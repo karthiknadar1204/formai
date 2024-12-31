52a028268f8802af516162b7eca2481527f11dae
karthik <karthiknadar1204@gmail.com>
2024-12-31 15:28:31 +0530

    logo fix

--- components/logo/index.tsx
+++ components/logo/index.tsx
@@ -12,12 +12,12 @@ const Logo = (props: { url?: string; color?: string }) => {
     >
       <Link href={url} className="flex items-center gap-2">
         <div className="relative flex items-center justify-center overflow-hidden size-[30px]">
-          <div className="absolute inset-0 bg-gradient-to-br from-blue-900 to-black rounded-lg transform rotate-45"></div>
-          <div className="absolute inset-0 bg-gradient-to-tl from-blue-700 to-transparent opacity-50 rounded-lg"></div>
+          <div className="absolute inset-0 bg-gradient-to-br from-blue-900 to-black rounded-lg transform rotate-45" />
+          <div className="absolute inset-0 bg-gradient-to-tl from-blue-700 to-transparent opacity-50 rounded-lg" />
           <span className="relative text-white font-bold italic font-mono" style={{ fontSize: "15px" }}>
             F
           </span>
-          <div className="absolute top-0 left-0 w-full h-full bg-white opacity-10 transform -skew-x-12"></div>
+          <div className="absolute top-0 left-0 w-full h-full bg-white opacity-10 transform -skew-x-12" />
         </div>
         <h5
           className={cn(
