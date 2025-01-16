ef91614f70030db7496da07716afa4fdbcdf6373
karthik <karthiknadar1204@gmail.com>
2025-01-16 17:46:16 +0530

    sidebar height fix

--- app/(routes)/dashboard/_components/BuilderSidebar.tsx
+++ app/(routes)/dashboard/_components/BuilderSidebar.tsx
@@ -31,12 +31,12 @@ const BuilderSidebar = ({
 
   return (
     <Sidebar 
-      className="border-r fixed left-0 sm:left-[45px] pt-16 w-[85vw] sm:w-[300px] 
+      className="border-r fixed left-0 sm:left-[45px] top-16 pt-0 w-[85vw] sm:w-[300px] 
         bg-white z-50 h-[calc(100vh_-_64px)]" 
       {...rest}
     >
       <SidebarHeader className="bg-white px-0 sticky top-0">
-        <header className="border-b border-gray-200 w-full pt-1 pb-2 flex shrink-0 items-center gap-2">
+        <header className="border-b border-gray-200 w-full pt-3 pb-2 flex shrink-0 items-center gap-2">
           <div className="flex items-center gap-2 px-3 sm:px-4 overflow-hidden">
             <Home className="min-w-4 h-4" />
             <Separator orientation="vertical" className="mr-2 h-4 hidden sm:block" />
@@ -60,7 +60,7 @@ const BuilderSidebar = ({
         </header>
       </SidebarHeader>
 
-      <SidebarContent className="pt-2 px-3 sm:px-5 bg-white h-full overflow-y-auto">
+      <SidebarContent className="pt-2 px-3 sm:px-5 bg-white h-[calc(100vh_-_128px)] overflow-y-auto">
         <div className="w-full">
           <div className="w-full flex flex-row gap-1 h-[39px] rounded-full bg-gray-100 p-1">
             <button
